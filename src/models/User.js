dc67827b4c5d32032d591fcf3c23ff02f778ebdf
Carlos Kauan <carloskauanmoreiradesousa@gmail.com>
2020-09-09 20:12:08 -0300

    v0.0.1: adding models  (model mvc)

---
+++ src/models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate');
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    buy: {
+        type: Number,
+        required: true,
+    },
+    notes: {
+        type: String,
+        default: ''
+    },
+    created_at: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+UserSchema.plugin(mongoosePaginate);
+
+mongoose.model('Users', UserSchema);
