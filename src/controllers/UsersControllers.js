4ac4a9649a8d9578f76d69509428c7b0eeae2ae3
Carlos Kauan <carloskauanmoreiradesousa@gmail.com>
2020-09-11 19:35:01 -0300

    v:0.0.3: cleaning code & configuring routes

--- src/controllers/UsersControllers.js
+++ src/controllers/UsersControllers.js
@@ -1,27 +1,57 @@
 const mongoose = require('mongoose');
 const User = mongoose.model('Users');
+const userName = "Carlos Kauãn";
+
 module.exports = {
 
     async main(req, res) {
-        const users = await User.find({ buy: { $gte: 500 } });
-        // users < 1 ? console.log('nenhum usuario') : console.log(users.length + ' usuarios');
-        return res.render('layout.njk')
-        // return res.json(users);
+        const [loyalQtd, usersQtd] = await getAwardedUsers();
+
+        const resources = {
+            pageTitle: "Dashboard",
+            loyalQtd,
+            usersQtd,
+            userName
+        }
+
+        return res.render('dashboard.njk', resources);
     },
+
     async index(req, res) {
         const { page = 1 } = req.query;
-        const users = await User.paginate({}, { page, limit: 10 });
-        return res.json(users);
+        const [loyalQtd, usersQtd] = await getAwardedUsers();
+        const usersList = await User.paginate({}, { page, limit: 10 });
+        const resources = {
+            pageTitle: "Usuários",
+            loyalQtd,
+            usersQtd,
+            usersList,
+            userName
+        };
+        return res.render('users.njk', resources);
+    },
+
+    async form(req, res) {
+        const [loyalQtd, usersQtd] = await getAwardedUsers();
+        const resources = {
+            pageTitle: "Novo Usuário",
+            loyalQtd,
+            usersQtd,
+            userName
+        };
+        return res.render('create.njk', resources);
     },
     async create(req, res) {
-        const users = await User.create(req.body);
-        console.log(req.body.star)
-        return res.json(users);
+        req.body.buy = req.body.buy.replace(',', '.');
+        await User.create(req.body);
+        return res.redirect('/users');
+
     },
     async show(req, res) {
         const users = await User.findById(req.params.id);
         return res.json(users);
     },
+
     async update(req, res) {
         let amount;
         if (req.body.sum) {
@@ -33,8 +63,19 @@ module.exports = {
         const users = await User.findByIdAndUpdate(req.params.id, { ...req.body, buy: amount }, { new: true });
         return res.json(users);
     },
+
     async destroy(req, res) {
         await User.findByIdAndRemove(req.params.id);
-        return res.send("Well done");
     }
+}
+
+async function getAwardedUsers() {
+    const users = await User.find();
+    const loyalQtd = [];
+    users.forEach((el) => {
+        if (el.buy >= 500) {
+            loyalQtd.push(el.buy)
+        }
+    })
+    return [loyalQtd.length, users.length];
 }
--- src/controllers/routes.js
+++ src/controllers/routes.js
@@ -4,7 +4,8 @@ const ProductController = require('./UsersControllers');
 
 routes.get('/', ProductController.main);
 routes.get('/users', ProductController.index);
-routes.get('/users/:id', ProductController.show);
+routes.get('/users/show/:id', ProductController.show);
+routes.get('/users/create', ProductController.form);
 routes.post('/users/create', ProductController.create);
 routes.put('/users/:id', ProductController.update);
 routes.delete('/users/:id', ProductController.destroy);
