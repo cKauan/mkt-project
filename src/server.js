f76a37c473f15dd9053d5ce52360a7c4a7125a26
Carlos Kauan <carloskauanmoreiradesousa@gmail.com>
2020-09-13 17:25:34 -0300

    v0.0.4: configuring passport.js

--- src/server.js
+++ src/server.js
@@ -3,21 +3,36 @@ const nunjucks = require('nunjucks');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 const dotenv = require('dotenv');
+const methodOverride = require('method-override');
+const passport = require('passport');
+const cookieSession = require('cookie-session');
 const app = express();
 dotenv.config();
 
+require('./controllers/passport-auth');
 const URL = process.env.MONGODB_URI;
 mongoose.connect(URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false
 });
+
 requireDir('./models')
 
+app.use(cookieSession({
+    name: 'user-auth',
+    keys: ['key', 'key2']
+}))
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.set('view engine', 'njk');
+app.use(methodOverride("_method"));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/', require('./controllers/routes'));
 
 nunjucks.configure('src/views', {
     express: app,
@@ -25,5 +40,4 @@ nunjucks.configure('src/views', {
     noCache: true
 })
 
-app.use('/', require('./controllers/routes'))
 app.listen(process.env.PORT || 3333);
