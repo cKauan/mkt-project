8374356585014c46e7e040a3c1cc5e2ca676dfc2
Carlos Kauan <carloskauanmoreiradesousa@gmail.com>
2020-09-14 14:18:01 -0300

    v0.0.5: Deploying app to heroku

--- src/controllers/passport-auth.js
+++ src/controllers/passport-auth.js
@@ -11,7 +11,7 @@ passport.deserializeUser((user, done) => {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3333/login/callback"
+    callbackURL: process.env.CALLBACK_URL
 }, (accessToken, refreshToken, profile, done) => {
     return done(null, profile);
 }
