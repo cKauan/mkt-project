b852bb6b63a8243ec4f11c00c0c7349747bf92fb
Carlos Kauan <carloskauanmoreiradesousa@gmail.com>
2020-09-14 14:06:41 -0300

    v0.0.5: deploying to heroku

--- src/controllers/utils.js
+++ src/controllers/utils.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const User = mongoose.model('Users');
-
 exports.convertFormatNumberToDBNumber = (number) => {
     number = number.replace('.', '');
     number = number.replace(',', '.');
